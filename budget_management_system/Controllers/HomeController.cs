90d453d66f70cd766e524aa4e0e030628c1dd923
Wesdell <wesdell17@gmail.com>
2024-04-06 15:34:13 -0500

    Remove method to fire deleted Privacy page.

--- budget_management_system/Controllers/HomeController.cs
+++ budget_management_system/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace budget_management_system.Controllers
 			return View();
 		}
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
