3cdee1052a8177f7d60016e43c8816fabb1b9bf6
Wesdell <wesdell17@gmail.com>
2024-04-07 14:19:34 -0500

    Create interface for AccountType data.

---
+++ budget_management_system/Interfaces/AccountType.cs
@@ -0,0 +1,10 @@
+﻿namespace budget_management_system.Interfaces
+{
+	public interface IAccountType
+	{
+		int Id { get; set; }
+		string Name { get; set; }
+		int Order { get; set; }
+		int UserId { get; set; }
+	}
+}
