8e296ff0e654f5882b8006d5c896514a92b90e27
Wesdell <wesdell17@gmail.com>
2024-04-07 15:27:06 -0500

    Add validations from AccountType model.

--- budget_management_system/Models/AccountTypeModel.cs
+++ budget_management_system/Models/AccountTypeModel.cs
@@ -7,8 +7,6 @@ namespace budget_management_system.Models
 	{
 		public int Id { get; set; }
 		[Required(ErrorMessage = "{0} field is required.")]
-		[StringLength(maximumLength: 50, MinimumLength = 2, ErrorMessage = "{0} field must contain {2} - {1} characters.")]
-		[Display(Name = "Account type")]
 		public string Name { get; set; }
 		public int Order { get; set; }
 		public int UserId { get; set; }
