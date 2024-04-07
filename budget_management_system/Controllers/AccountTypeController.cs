4e870ccdd8dee99a5d2ecd894f2f50c080fb30c4
Wesdell <wesdell17@gmail.com>
2024-04-07 14:22:33 -0500

    Add controller and method to fire AccountType actions.

---
+++ budget_management_system/Controllers/AccountTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace budget_management_system.Controllers
+{
+	public class AccountTypeController : Controller
+	{
+		public IActionResult CreateAccountType()
+		{
+			return View();
+		}
+	}
+}
