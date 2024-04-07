141285ccd16e06fa8f897a913a72c5c0b203fd88
Wesdell <wesdell17@gmail.com>
2024-04-07 14:20:19 -0500

    Add AccountType model properties.

---
+++ budget_management_system/Models/AccountTypeModel.cs
@@ -0,0 +1,12 @@
+﻿using budget_management_system.Interfaces;
+
+namespace budget_management_system.Models
+{
+	public class AccountTypeModel : IAccountType
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Order { get; set; }
+		public int UserId { get; set; }
+	}
+}
