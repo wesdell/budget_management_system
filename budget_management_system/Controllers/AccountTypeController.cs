437ea9c017f6563bc356440eaf05c99525a6659b
Wesdell <wesdell17@gmail.com>
2024-04-07 14:32:29 -0500

    New method to receive form data from AccountType page.

--- budget_management_system/Controllers/AccountTypeController.cs
+++ budget_management_system/Controllers/AccountTypeController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using budget_management_system.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace budget_management_system.Controllers
 {
 	public class AccountTypeController : Controller
 	{
+		[HttpGet]
 		public IActionResult CreateAccountType()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult CreateAccountType(AccountTypeModel accountType)
 		{
 			return View();
 		}
