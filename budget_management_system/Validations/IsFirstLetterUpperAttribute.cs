a0ddf8037cd44a3a749168c531441eacef60c3fb
Wesdell <wesdell17@gmail.com>
2024-04-08 14:28:39 -0500

    Create custom attribute validations.

---
+++ budget_management_system/Validations/IsFirstLetterUpperAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace budget_management_system.Validations
+{
+	public class IsFirstLetterUpperAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value == null || string.IsNullOrEmpty(value.ToString()))
+			{
+				return ValidationResult.Success;
+			}
+
+			string firstLetter = value.ToString()[0].ToString();
+			if (firstLetter != firstLetter.ToUpper())
+			{
+				return new ValidationResult("First letter must be on uppercase.");
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
