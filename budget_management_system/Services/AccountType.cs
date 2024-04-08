b1a9c411a7e7912d266240ff9abacf2a48d4f133
Wesdell <wesdell17@gmail.com>
2024-04-08 16:35:00 -0500

    Add service to create an account type.

---
+++ budget_management_system/Services/AccountType.cs
@@ -0,0 +1,25 @@
+﻿using budget_management_system.Interfaces;
+using budget_management_system.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace budget_management_system.Services
+{
+	public class AccountType : IAccountTypeDBActions
+	{
+		private readonly string connectionString;
+
+		public AccountType(IConfiguration configuration)
+		{
+			this.connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
+
+		public void CreateAccountType(AccountTypeModel accountType)
+		{
+			using SqlConnection connection = new SqlConnection(connectionString);
+			int id = connection.QuerySingle<int>($@"INSERT INTO AccountType (name, user_id, ""order"") VALUES (@Name, @UserId, 0); SELECT SCOPE_IDENTITY();", accountType);
+
+			accountType.Id = id;
+		}
+	}
+}
