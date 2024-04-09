e88022cbd8745542185c8b4e2ecfef9be7ef54a0
Wesdell <wesdell17@gmail.com>
2024-04-09 11:20:13 -0500

    Verify if an account already exists from client side.

--- budget_management_system/Models/AccountTypeModel.cs
+++ budget_management_system/Models/AccountTypeModel.cs
@@ -1,5 +1,6 @@
 ﻿using budget_management_system.Interfaces;
 using budget_management_system.Validations;
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 namespace budget_management_system.Models
@@ -9,6 +10,7 @@ namespace budget_management_system.Models
 		public int Id { get; set; }
 		[Required(ErrorMessage = "{0} field is required.")]
 		[IsFirstLetterUpper]
+		[Remote(action: "CheckAccountTypeAlreadyExists", controller: "AccountType")]
 		public string Name { get; set; }
 		public int Order { get; set; }
 		public int UserId { get; set; }
