1fa56a79bb8942dc1a91590dbe97dbbd05336ce7
Wesdell <wesdell17@gmail.com>
2024-04-09 14:44:09 -0500

    New endpoint to get User id.

---
+++ budget_management_system/Services/User.cs
@@ -0,0 +1,12 @@
+﻿using budget_management_system.Interfaces;
+
+namespace budget_management_system.Services
+{
+    public class User : IUserDBActions
+    {
+        public int GetUserId()
+        {
+            return 1;
+        }
+    }
+}
