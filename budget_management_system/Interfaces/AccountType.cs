53a569be56befdf5bedb200c72af29115b2a6dee
Wesdell <wesdell17@gmail.com>
2024-04-10 10:36:57 -0500

    New types for AccounTypeDBActions interface.

--- budget_management_system/Interfaces/AccountType.cs
+++ budget_management_system/Interfaces/AccountType.cs
@@ -2,18 +2,20 @@
 
 namespace budget_management_system.Interfaces
 {
-	public interface IAccountType
-	{
-		int Id { get; set; }
-		string Name { get; set; }
-		int Order { get; set; }
-		int UserId { get; set; }
-	}
+    public interface IAccountType
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        int Order { get; set; }
+        int UserId { get; set; }
+    }
 
-	public interface IAccountTypeDBActions
-	{
-		Task CreateAccountType(AccountTypeModel accountType);
-		Task<bool> AccountAlreadyExists(string accountName, int userId);
-		Task<IEnumerable<AccountTypeModel>> GetAccountTypes(int userId);
-	}
+    public interface IAccountTypeDBActions
+    {
+        Task CreateAccountType(AccountTypeModel accountType);
+        Task<bool> AccountAlreadyExists(string accountName, int userId);
+        Task<IEnumerable<AccountTypeModel>> GetAccountTypes(int userId);
+        Task UpdateAccountType(AccountTypeModel accountType);
+        Task<AccountTypeModel> GetAccountTypeById(int id, int userId);
+    }
 }
