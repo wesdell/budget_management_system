aa4b74f21bc9ca03540adadf5a1197f2b2f643da
Wesdell <wesdell17@gmail.com>
2024-04-10 10:42:40 -0500

    Add methods to fire new AccountType endpoints.

--- budget_management_system/Controllers/AccountTypeController.cs
+++ budget_management_system/Controllers/AccountTypeController.cs
@@ -4,62 +4,85 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace budget_management_system.Controllers
 {
-    public class AccountTypeController : Controller
-    {
-        private readonly IAccountTypeDBActions _accountType;
-        private readonly IUserDBActions _userData;
+	public class AccountTypeController : Controller
+	{
+		private readonly IAccountTypeDBActions _accountType;
+		private readonly IUserDBActions _userData;
 
-        public AccountTypeController(IAccountTypeDBActions accountTypeData, IUserDBActions userData)
-        {
-            this._accountType = accountTypeData;
-            this._userData = userData;
-        }
+		public AccountTypeController(IAccountTypeDBActions accountTypeData, IUserDBActions userData)
+		{
+			this._accountType = accountTypeData;
+			this._userData = userData;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            IEnumerable<AccountTypeModel> accountTypes = await _accountType.GetAccountTypes(_userData.GetUserId());
-            return View(accountTypes);
-        }
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			IEnumerable<AccountTypeModel> accountTypes = await _accountType.GetAccountTypes(_userData.GetUserId());
+			return View(accountTypes);
+		}
 
-        [HttpGet]
-        public IActionResult CreateAccountType()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult CreateAccountType()
+		{
+			return View();
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> CheckAccountTypeAlreadyExists(string name)
-        {
-            bool accountTypeAlreadyExists = await _accountType.AccountAlreadyExists(name, _userData.GetUserId());
-            if (accountTypeAlreadyExists)
-            {
-                return Json($"{name} account already exists.");
-            }
-            return Json(true);
-        }
+		[HttpGet]
+		public async Task<IActionResult> UpdateAccountType(int id)
+		{
+			AccountTypeModel accountType = await this._accountType.GetAccountTypeById(id, this._userData.GetUserId());
+			if (accountType is null)
+			{
+				return RedirectToAction("NotFound", "Home");
+			}
+			return View(accountType);
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> CreateAccountType(AccountTypeModel accountTypeData)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(accountTypeData);
-            }
+		[HttpGet]
+		public async Task<IActionResult> CheckAccountTypeAlreadyExists(string name)
+		{
+			bool accountTypeAlreadyExists = await _accountType.AccountAlreadyExists(name, _userData.GetUserId());
+			if (accountTypeAlreadyExists)
+			{
+				return Json($"{name} account already exists.");
+			}
+			return Json(true);
+		}
 
-            accountTypeData.UserId = _userData.GetUserId();
+		[HttpPost]
+		public async Task<IActionResult> CreateAccountType(AccountTypeModel accountTypeData)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(accountTypeData);
+			}
 
-            bool recordExists = await _accountType.AccountAlreadyExists(accountTypeData.Name, accountTypeData.UserId);
+			accountTypeData.UserId = _userData.GetUserId();
 
-            if (recordExists)
-            {
-                ModelState.AddModelError(nameof(accountTypeData.Name), $"{accountTypeData.Name} account already exists.");
-                return View(accountTypeData);
-            }
+			bool recordExists = await _accountType.AccountAlreadyExists(accountTypeData.Name, accountTypeData.UserId);
 
-            await this._accountType.CreateAccountType(accountTypeData);
+			if (recordExists)
+			{
+				ModelState.AddModelError(nameof(accountTypeData.Name), $"{accountTypeData.Name} account already exists.");
+				return View(accountTypeData);
+			}
 
-            return RedirectToAction("Index");
-        }
-    }
+			await this._accountType.CreateAccountType(accountTypeData);
+
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateAccountType(AccountTypeModel accountTypeData)
+		{
+			AccountTypeModel accountType = await this._accountType.GetAccountTypeById(accountTypeData.Id, this._userData.GetUserId());
+			if (accountType is null)
+			{
+				return RedirectToAction("NotFound", "Home");
+			}
+			await this._accountType.UpdateAccountType(accountTypeData);
+			return RedirectToAction("Index");
+		}
+	}
 }
