2cd1ef95b94abd4c0a04dc9c6e311c480b28c88d
Wesdell <wesdell17@gmail.com>
2024-04-11 11:50:20 -0500

    New method to create an Account.

---
+++ budget_management_system/Services/Account.cs
@@ -0,0 +1,28 @@
+﻿using budget_management_system.Interfaces;
+using budget_management_system.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace budget_management_system.Services
+{
+	public class Account : IAccountDBActions
+	{
+		private readonly string connectionString;
+
+		public Account(IConfiguration configuration)
+		{
+			this.connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
+
+		public async Task CreateAccount(AccountModel account)
+		{
+			using SqlConnection connection = new SqlConnection(connectionString);
+			int id = await connection.QuerySingleAsync<int>(
+				@"INSERT INTO Account (name, description, balance, account_type_id) VALUES (@Name, @Description, @Balance, @AccountTypeId); SELECT SCOPE_IDENTITY()",
+				account
+				);
+
+			account.Id = id;
+		}
+	}
+}
