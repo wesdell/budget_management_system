45efe370561d6ac9d270494451a050ac19a9dac4
Wesdell <wesdell17@gmail.com>
2024-04-11 14:45:50 -0500

    New method to get accounts data.

--- budget_management_system/Services/Account.cs
+++ budget_management_system/Services/Account.cs
@@ -7,22 +7,33 @@ namespace budget_management_system.Services
 {
 	public class Account : IAccountDBActions
 	{
-		private readonly string connectionString;
+		private readonly string _connectionString;
 
 		public Account(IConfiguration configuration)
 		{
-			this.connectionString = configuration.GetConnectionString("DefaultConnection");
+			this._connectionString = configuration.GetConnectionString("DefaultConnection");
 		}
 
 		public async Task CreateAccount(AccountModel account)
 		{
-			using SqlConnection connection = new SqlConnection(connectionString);
+			using SqlConnection connection = new SqlConnection(this._connectionString);
 			int id = await connection.QuerySingleAsync<int>(
 				@"INSERT INTO Account (name, description, balance, account_type_id) VALUES (@Name, @Description, @Balance, @AccountTypeId); SELECT SCOPE_IDENTITY()",
 				account
 				);
 
 			account.Id = id;
+		}
+
+		public async Task<IEnumerable<AccountModel>> GetAccounts(int userId)
+		{
+			using SqlConnection connection = new SqlConnection(this._connectionString);
+			return await connection.QueryAsync<AccountModel>(
+				@"SELECT acc.id, acc.name, acc.balance, acct.name AS account_type FROM Account AS acc
+				INNER JOIN AccountType AS acct ON acc.account_type_id = acct.id
+				WHERE acct.user_id = @UserId ORDER BY acct.""order""",
+				new { UserId = userId }
+				);
 		}
 	}
 }
