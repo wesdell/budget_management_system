31cd7f5bc4840da990b5daf70990a76488453298
Wesdell <wesdell17@gmail.com>
2024-04-11 11:52:21 -0500

    New endpoint to create an Account and extract GetAccountTypes in a method.

--- budget_management_system/Controllers/AccountController.cs
+++ budget_management_system/Controllers/AccountController.cs
@@ -9,20 +9,47 @@ namespace budget_management_system.Controllers
 	{
 		private readonly IAccountTypeDBActions _accountType;
 		private readonly IUserDBActions _user;
+		private readonly IAccountDBActions _account;
 
-		public AccountController(IAccountTypeDBActions accountType, IUserDBActions user)
+		public AccountController(IAccountTypeDBActions accountType, IUserDBActions user, IAccountDBActions account)
 		{
 			this._accountType = accountType;
 			this._user = user;
+			this._account = account;
 		}
 
 		[HttpGet]
 		public async Task<ActionResult> CreateAccount()
 		{
-			IEnumerable<AccountTypeModel> accountTypes = await _accountType.GetAccountTypes(_user.GetUserId());
 			CreateAccountViewModel model = new CreateAccountViewModel();
-			model.AccountList = accountTypes.Select(account => new SelectListItem(account.Name, account.Id.ToString()));
+			model.AccountList = await this.GetAccountTypes(_user.GetUserId());
 			return View(model);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateAccount(CreateAccountViewModel account)
+		{
+			AccountTypeModel accountType = await _accountType.GetAccountTypeById(account.AccountTypeId, _user.GetUserId());
+
+			if (accountType is null)
+			{
+				return RedirectToAction("NotFound", "Home");
+			}
+
+			if (!ModelState.IsValid)
+			{
+				account.AccountList = await this.GetAccountTypes(_user.GetUserId());
+				return View(account);
+			}
+
+			await _account.CreateAccount(account);
+			return RedirectToAction("Index");
+		}
+
+		private async Task<IEnumerable<SelectListItem>> GetAccountTypes(int userId)
+		{
+			IEnumerable<AccountTypeModel> accountTypes = await _accountType.GetAccountTypes(userId);
+			return accountTypes.Select(account => new SelectListItem(account.Name, account.Id.ToString()));
 		}
 	}
 }
