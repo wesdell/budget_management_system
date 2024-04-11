77b90bc39eaf740a2bea306262dbadee732bc630
Wesdell <wesdell17@gmail.com>
2024-04-11 11:19:01 -0500

    New interface to type Accounts.

---
+++ budget_management_system/Interfaces/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace budget_management_system.Interfaces
+{
+	public interface IAccount
+	{
+		int Id { get; set; }
+		string Name { get; set; }
+		decimal Balance { get; set; }
+		string Description { get; set; }
+		int AccountTypeId { get; set; }
+	}
+}
