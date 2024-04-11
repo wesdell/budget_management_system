3b189f2139ca57c664faa2abb92f3f09565161d8
Wesdell <wesdell17@gmail.com>
2024-04-11 11:21:07 -0500

    New endpoint to fire CreateAccount view.

---
+++ budget_management_system/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using budget_management_system.Interfaces;
+using budget_management_system.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace budget_management_system.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly IAccountTypeDBActions _accountType;
+		private readonly IUserDBActions _user;
+
+		public AccountController(IAccountTypeDBActions accountType, IUserDBActions user)
+		{
+			this._accountType = accountType;
+			this._user = user;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult> CreateAccount()
+		{
+			IEnumerable<AccountTypeModel> accountTypes = await _accountType.GetAccountTypes(_user.GetUserId());
+			CreateAccountViewModel model = new CreateAccountViewModel();
+			model.AccountList = accountTypes.Select(account => new SelectListItem(account.Name, account.Id.ToString()));
+			return View(model);
+		}
+	}
+}
