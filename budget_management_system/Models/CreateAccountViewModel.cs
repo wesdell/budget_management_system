709edb5656c28a4946255c55848a37eac3f47018
Wesdell <wesdell17@gmail.com>
2024-04-11 11:20:18 -0500

    New model to work with Account model data in the view.

---
+++ budget_management_system/Models/CreateAccountViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace budget_management_system.Models
+{
+	public class CreateAccountViewModel : AccountModel
+	{
+		public IEnumerable<SelectListItem> AccountList { get; set; }
+	}
+}
