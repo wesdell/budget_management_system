009f51c63f750222c6eaeb7969b1edc617d365f8
Wesdell <wesdell17@gmail.com>
2024-04-11 14:47:37 -0500

    New model to send Account data to Index page.

---
+++ budget_management_system/Models/AccountIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace budget_management_system.Models
+{
+	public class AccountIndexViewModel
+	{
+		public string AccountType { get; set; }
+		public IEnumerable<AccountModel> Accounts { get; set; }
+		public decimal TotalBalance => Accounts.Sum(x => x.Balance);
+
+	}
+}
