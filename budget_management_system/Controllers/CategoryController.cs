adbb4e4adf812dc1a6960157db9ed00e6dae47b1
Wesdell <wesdell17@gmail.com>
2024-04-12 14:53:53 -0500

    New endpoints to fire Category page and to create a category.

---
+++ budget_management_system/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using budget_management_system.Interfaces;
+using budget_management_system.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace budget_management_system.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly IUserDBActions _userService;
+		private readonly ICategoryDBActions _categoryService;
+
+		public CategoryController(IUserDBActions user, ICategoryDBActions category)
+		{
+			this._userService = user;
+			this._categoryService = category;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> CreateCategory()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateCategory(CategoryModel category)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(category);
+			}
+
+			category.Id = this._userService.GetUserId();
+			await this._categoryService.CreateCategory(category);
+			return RedirectToAction("Index");
+		}
+	}
+}
