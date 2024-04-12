5cd3fbfe9008de5c556a927bed0a14faf01fcd99
Wesdell <wesdell17@gmail.com>
2024-04-12 14:54:51 -0500

    Add model to display Category data.

---
+++ budget_management_system/Models/CategoryModel.cs
@@ -0,0 +1,17 @@
+﻿using budget_management_system.Interfaces;
+using budget_management_system.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace budget_management_system.Models
+{
+	public class CategoryModel : ICategory
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Category {0} is required.")]
+		[StringLength(maximumLength: 50)]
+		[IsFirstLetterUpper]
+		public string Name { get; set; }
+		public TransactionType TransactionTypeId { get; set; }
+		public int UserId { get; set; }
+	}
+}
