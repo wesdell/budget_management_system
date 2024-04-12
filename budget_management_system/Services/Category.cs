c6c5fb5e738e7b67ef12d55fd11a18c8332d678f
Wesdell <wesdell17@gmail.com>
2024-04-12 14:52:45 -0500

    New method to create a category.

---
+++ budget_management_system/Services/Category.cs
@@ -0,0 +1,27 @@
+﻿using budget_management_system.Interfaces;
+using budget_management_system.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace budget_management_system.Services
+{
+	public class Category : ICategoryDBActions
+	{
+		private readonly string _connectionString;
+
+		public Category(IConfiguration configuration)
+		{
+			this._connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
+
+		public async Task CreateCategory(CategoryModel category)
+		{
+			using SqlConnection connection = new SqlConnection(this._connectionString);
+			int id = await connection.QuerySingleAsync<int>(
+				@"INSERT INTO Category (name, transaction_type_id, user_id) VALUES (@Name, @TransactionTypeId, @UserId); SELECT SCOPE_IDENTITY()",
+				category
+				);
+			category.Id = id;
+		}
+	}
+}
