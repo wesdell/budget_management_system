b572de685510c2d7cd74ad3a78ca3659d9cde31a
Wesdell <wesdell17@gmail.com>
2024-04-12 11:28:34 -0500

    New methods to get an account by id and to update an account.

--- budget_management_system/Services/Account.cs
+++ budget_management_system/Services/Account.cs
@@ -31,8 +31,28 @@ namespace budget_management_system.Services
 			return await connection.QueryAsync<AccountModel>(
 				@"SELECT Account.id, Account.name, Account.balance, AccountType.name AS AccountType FROM Account
 				INNER JOIN AccountType ON AccountType.id = Account.account_type_id
-				WHERE AccountType.user_id = 1 ORDER BY AccountType.""order""",
+				WHERE AccountType.user_id = @UserId ORDER BY AccountType.""order""",
 				new { UserId = userId }
+				);
+		}
+
+		public async Task<AccountModel> GetAccountById(int id, int userId)
+		{
+			using SqlConnection connection = new SqlConnection(this._connectionString);
+			return await connection.QueryFirstOrDefaultAsync<AccountModel>(
+				@"SELECT Account.id, Account.name, Account.balance, Account.description, Account.account_type_id AS AccountTypeId FROM Account
+				INNER JOIN AccountType ON AccountType.id = Account.account_type_id
+				WHERE AccountType.user_id = @UserId AND Account.id = @Id",
+				new { Id = id, UserId = userId }
+				);
+		}
+
+		public async Task UpdateAccount(CreateAccountViewModel account)
+		{
+			using SqlConnection connection = new SqlConnection(this._connectionString);
+			await connection.ExecuteAsync(
+				@"UPDATE Account SET name = @Name, balance = @Balance, description = @Description, account_type_id = @AccountTypeId WHERE id = @Id",
+				account
 				);
 		}
 	}
