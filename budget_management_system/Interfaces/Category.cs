07be097c620ae52fdb4b414ee46eaaf2f0818869
Wesdell <wesdell17@gmail.com>
2024-04-12 14:52:13 -0500

    New types for Category interface.

---
+++ budget_management_system/Interfaces/Category.cs
@@ -0,0 +1,17 @@
+﻿using budget_management_system.Models;
+
+namespace budget_management_system.Interfaces
+{
+	public interface ICategory
+	{
+		int Id { get; set; }
+		string Name { get; set; }
+		TransactionType TransactionTypeId { get; set; }
+		int UserId { get; set; }
+	}
+
+	public interface ICategoryDBActions
+	{
+		Task CreateCategory(CategoryModel category);
+	}
+}
