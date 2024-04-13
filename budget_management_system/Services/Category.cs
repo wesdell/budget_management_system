2f8b2e7bd21573d0a56a3a3b8249b3e7d894f88a
Wesdell <wesdell17@gmail.com>
2024-04-13 14:54:26 -0500

    Add method to update a category.

--- budget_management_system/Services/Category.cs
+++ budget_management_system/Services/Category.cs
@@ -32,5 +32,23 @@ namespace budget_management_system.Services
 				new { UserId = userId }
 				);
 		}
+
+		public async Task<CategoryModel> GetCategoryById(int id, int userId)
+		{
+			using SqlConnection connection = new SqlConnection(this._connectionString);
+			return await connection.QueryFirstOrDefaultAsync<CategoryModel>(
+				@"SELECT id, name, transaction_type_id AS TransactionTypeId, user_id AS UserId FROM Category WHERE id = @Id AND user_id = @UserId",
+				new { Id = id, UserId = userId }
+				);
+		}
+
+		public async Task UpdateCategory(CategoryModel category)
+		{
+			using SqlConnection connection = new SqlConnection(this._connectionString);
+			await connection.ExecuteAsync(
+				@"UPDATE Category SET name = @Name, transaction_type_id = @TransactionTypeId WHERE id = @Id",
+				category
+				);
+		}
 	}
 }
