e2c06edd3fa6be15c7779d7cac39a49d0b46b345
Wesdell <wesdell17@gmail.com>
2024-04-14 14:46:42 -0500

    New model to interact with Transaction data.

---
+++ budget_management_system/Models/TransactionModel.cs
@@ -0,0 +1,18 @@
+﻿using budget_management_system.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace budget_management_system.Models
+{
+	public class TransactionModel : ITransaction
+	{
+		public int Id { get; set; }
+		[StringLength(maximumLength: 150)]
+		public string Description { get; set; }
+		public decimal Amount { get; set; }
+		public DateTime CreatedAt { get; set; } = DateTime.Today;
+		public int UserId { get; set; }
+		public int AccountId { get; set; }
+		[Range(1, maximum: int.MaxValue)]
+		public int CategoryId { get; set; }
+	}
+}
