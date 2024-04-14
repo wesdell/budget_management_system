e46a2b9181b6ba78cb07e1b506425d7564fa660a
Wesdell <wesdell17@gmail.com>
2024-04-14 14:48:02 -0500

    Add method to create a transaction from db.

---
+++ budget_management_system/Services/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using budget_management_system.Interfaces;
+using budget_management_system.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace budget_management_system.Services
+{
+	public class Transaction : ITransactionDBActions
+	{
+		private readonly string _connectionString;
+
+		public Transaction(IConfiguration configuration)
+		{
+			this._connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
+
+		public async Task CreateTransaction(TransactionModel transaction)
+		{
+			using SqlConnection connection = new SqlConnection(this._connectionString);
+			int id = await connection.QuerySingleAsync<int>(
+				"CreateTransaction",
+				new
+				{
+					description = transaction.Description,
+					amount = transaction.Amount,
+					created_at = transaction.CreatedAt,
+					user_id = transaction.UserId,
+					account_id = transaction.AccountId,
+					category_id = transaction.CategoryId
+				},
+				commandType: System.Data.CommandType.StoredProcedure
+				);
+			transaction.Id = id;
+		}
+	}
+}
