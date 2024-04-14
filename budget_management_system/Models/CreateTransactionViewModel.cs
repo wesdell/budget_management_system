8506246cf290654f0162e40dcd110056e6bdc19a
Wesdell <wesdell17@gmail.com>
2024-04-14 14:47:22 -0500

    New model to send transaction data from controller to view.

---
+++ budget_management_system/Models/CreateTransactionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace budget_management_system.Models
+{
+	public class CreateTransactionViewModel : TransactionModel
+	{
+		public IEnumerable<SelectListItem> AccountList { get; set; }
+		public IEnumerable<CategoryModel> CategoryList { get; set; }
+	}
+}
