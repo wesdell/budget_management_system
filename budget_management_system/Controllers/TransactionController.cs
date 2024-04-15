cd1bb0aa14766cfc6dc9d72403c7d21b9ae2f029
Wesdell <wesdell17@gmail.com>
2024-04-15 16:44:37 -0500

    New endpoint and method to get categories by user and transaction type.

--- budget_management_system/Controllers/TransactionController.cs
+++ budget_management_system/Controllers/TransactionController.cs
@@ -11,25 +11,43 @@ namespace budget_management_system.Controllers
 		private readonly ITransactionDBActions _transactionService;
 		private readonly IUserDBActions _userService;
 		private readonly IAccountDBActions _accountService;
+		private readonly ICategoryDBActions _categoryService;
 
-		public TransactionController(ITransactionDBActions transaction, IUserDBActions user, IAccountDBActions account)
+		public TransactionController(ITransactionDBActions transaction, IUserDBActions user, IAccountDBActions account, ICategoryDBActions category)
 		{
 			this._transactionService = transaction;
 			this._userService = user;
 			this._accountService = account;
+			this._categoryService = category;
 		}
 
+		[HttpGet]
 		public async Task<IActionResult> CreateTransaction()
 		{
 			CreateTransactionViewModel model = new CreateTransactionViewModel();
 			model.AccountList = await this.GetAccounts(this._userService.GetUserId());
+			model.CategoryList = await this.GetCategories(this._userService.GetUserId(), model.TransactionTypeId);
 			return View(model);
 		}
 
+		[HttpGet]
 		public async Task<IEnumerable<SelectListItem>> GetAccounts(int userId)
 		{
 			IEnumerable<AccountModel> accounts = await this._accountService.GetAccounts(userId);
 			return accounts.Select(account => new SelectListItem(account.Name, account.Id.ToString()));
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> GetCategories([FromBody] ETransactionType transactionType)
+		{
+			IEnumerable<SelectListItem> categories = await this.GetCategories(this._userService.GetUserId(), transactionType);
+			return Ok(categories);
+		}
+
+		private async Task<IEnumerable<SelectListItem>> GetCategories(int userId, ETransactionType transactionType)
+		{
+			IEnumerable<CategoryModel> categories = await this._categoryService.GetCategories(userId, transactionType);
+			return categories.Select(category => new SelectListItem(category.Name, category.Id.ToString()));
 		}
 	}
 }
