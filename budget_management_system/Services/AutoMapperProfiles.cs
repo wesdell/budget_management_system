50b9949615a68e1e77620697cb1d95f8ccc2491b
Wesdell <wesdell17@gmail.com>
2024-04-17 16:01:49 -0500

    New method to map from one model to another.

---
+++ budget_management_system/Services/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using budget_management_system.Models;
+
+namespace budget_management_system.Services
+{
+	public class AutoMapperProfiles : Profile
+	{
+		public AutoMapperProfiles()
+		{
+			CreateMap<AccountModel, CreateAccountViewModel>();
+			CreateMap<UpdateTransactionViewModel, TransactionModel>().ReverseMap();
+		}
+	}
+}
