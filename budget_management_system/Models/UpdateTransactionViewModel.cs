6f7e0c0c0455194677a66cabb5b33865e2aefdfc
Wesdell <wesdell17@gmail.com>
2024-04-17 15:11:41 -0500

    New model to send data from TransactionController to UpdateTransaction view .

---
+++ budget_management_system/Models/UpdateTransactionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace budget_management_system.Models
+{
+	public class UpdateTransactionViewModel : CreateTransactionViewModel
+	{
+		public int PreviousAccountId { get; set; }
+		public decimal PreviousAmount { get; set; }
+	}
+}
