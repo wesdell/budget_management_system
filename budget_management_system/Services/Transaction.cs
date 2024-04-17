28edc5cb432eb678b8de1d6a35b551de43e35a6e
Wesdell <wesdell17@gmail.com>
2024-04-17 15:12:13 -0500

    New methods to update a transaction and to get a transaction by id.

--- budget_management_system/Services/Transaction.cs
+++ budget_management_system/Services/Transaction.cs
@@ -32,5 +32,40 @@ namespace budget_management_system.Services
 				);
 			transaction.Id = id;
 		}
+
+		public async Task<TransactionModel> GetTransactionById(int id, int userId)
+		{
+			using SqlConnection connection = new SqlConnection(this._connectionString);
+			return await connection.QueryFirstOrDefaultAsync<TransactionModel>(
+				@"SELECT ""Transaction"".id AS Id, ""Transaction"".user_id AS UserId,
+				""Transaction"".account_id AS AccountId, ""Transaction"".category_id AS CategoryId,
+				""Transaction"".amount AS Amount, ""Transaction"".created_at AS CreatedAt,
+				""Transaction"".description AS ""Description"", Category.transaction_type_id AS TransactionTypeId
+				FROM ""Transaction""
+				INNER JOIN Category ON Category.id = ""Transaction"".category_id
+				WHERE ""Transaction"".id = @Id AND ""Transaction"".user_id = @UserId",
+				new { Id = id, UserId = userId }
+				);
+		}
+
+		public async Task UpdateTransaction(TransactionModel transaction, int previosAccountId, int previousAmount)
+		{
+			using SqlConnection connection = new SqlConnection(this._connectionString);
+			await connection.ExecuteAsync(
+				"UpdateTransaction",
+				new
+				{
+					id = transaction.Id,
+					created_at = transaction.CreatedAt,
+					amount = transaction.Amount,
+					previous_amount = previousAmount,
+					account_id = transaction.AccountId,
+					previous_account_id = previosAccountId,
+					category_id = transaction.CategoryId,
+					description = transaction.Description
+				},
+				commandType: System.Data.CommandType.StoredProcedure
+				);
+		}
 	}
 }
