71d6cb1dbb3a67e238fe4405559cae060e8d2a6d
Wesdell <wesdell17@gmail.com>
2024-04-17 16:09:16 -0500

    New endpoints to fire UpdateTransaction page and to update a transaction.

--- budget_management_system/Controllers/TransactionController.cs
+++ budget_management_system/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
-﻿using budget_management_system.Interfaces;
+﻿using AutoMapper;
+using budget_management_system.Interfaces;
 using budget_management_system.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -12,13 +13,15 @@ namespace budget_management_system.Controllers
 		private readonly IUserDBActions _userService;
 		private readonly IAccountDBActions _accountService;
 		private readonly ICategoryDBActions _categoryService;
+		private readonly IMapper _mapper;
 
-		public TransactionController(ITransactionDBActions transaction, IUserDBActions user, IAccountDBActions account, ICategoryDBActions category)
+		public TransactionController(ITransactionDBActions transaction, IUserDBActions user, IAccountDBActions account, ICategoryDBActions category, IMapper mapper)
 		{
 			this._transactionService = transaction;
 			this._userService = user;
 			this._accountService = account;
 			this._categoryService = category;
+			this._mapper = mapper;
 		}
 
 		[HttpGet]
@@ -33,6 +36,29 @@ namespace budget_management_system.Controllers
 			CreateTransactionViewModel model = new CreateTransactionViewModel();
 			model.AccountList = await this.GetAccounts(this._userService.GetUserId());
 			model.CategoryList = await this.GetCategories(this._userService.GetUserId(), model.TransactionTypeId);
+			return View(model);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> UpdateTransaction(int id)
+		{
+			TransactionModel transaction = await this._transactionService.GetTransactionById(id, this._userService.GetUserId());
+			if (transaction is null)
+			{
+				return RedirectToAction("NotFound", "Home");
+			}
+
+			UpdateTransactionViewModel model = this._mapper.Map<UpdateTransactionViewModel>(transaction);
+
+			model.PreviousAmount = model.Amount;
+			if (model.TransactionTypeId == ETransactionType.Expense)
+			{
+				model.PreviousAmount = model.Amount * -1;
+			}
+
+			model.PreviousAccountId = transaction.AccountId;
+			model.AccountList = await this.GetAccounts(this._userService.GetUserId());
+			model.CategoryList = await this.GetCategories(this._userService.GetUserId(), transaction.TransactionTypeId);
 			return View(model);
 		}
 
@@ -65,6 +91,39 @@ namespace budget_management_system.Controllers
 			}
 
 			await this._transactionService.CreateTransaction(transactionModel);
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateTransaction(UpdateTransactionViewModel transactionModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				transactionModel.AccountList = await this.GetAccounts(this._userService.GetUserId());
+				transactionModel.CategoryList = await this.GetCategories(this._userService.GetUserId(), transactionModel.TransactionTypeId);
+				return View(transactionModel);
+			}
+
+			AccountModel account = await this._accountService.GetAccountById(transactionModel.AccountId, this._userService.GetUserId());
+			if (account is null)
+			{
+				return RedirectToAction("NotFound", "Home");
+			}
+
+			CategoryModel category = await this._categoryService.GetCategoryById(transactionModel.CategoryId, this._userService.GetUserId());
+			if (category is null)
+			{
+				return RedirectToAction("NotFound", "Home");
+			}
+
+			TransactionModel newTransaction = this._mapper.Map<TransactionModel>(transactionModel);
+
+			if (transactionModel.TransactionTypeId == ETransactionType.Expense)
+			{
+				newTransaction.Amount *= -1;
+			}
+
+			await this._transactionService.UpdateTransaction(newTransaction, transactionModel.PreviousAccountId, transactionModel.PreviousAmount);
 			return RedirectToAction("Index");
 		}
 
