1785dbc30a4d5ed02872f0b9318db91b44a2de9b
Wesdell <wesdell17@gmail.com>
2024-04-17 16:24:15 -0500

    Extract functionality to a service.

---
+++ budget_management_system/wwwroot/js/Transaction.js
@@ -0,0 +1,18 @@
+﻿function initTransactionForm(url) {
+    $("#TransactionTypeId").change(async function () {
+        const $selectedValue = $(this).val();
+
+        const res = await fetch(url, {
+            method: "POST",
+            body: $selectedValue,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        const json = await res.json();
+
+        const $options = json.map(el => `<option value=${el.value}>${el.text}</option>`);
+        $("#CategoryId").html($options);
+    })
+}
