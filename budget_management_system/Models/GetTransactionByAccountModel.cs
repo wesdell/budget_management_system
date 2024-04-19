2a216fc4b424a8acea8d9fafda93569cee4950fe
Wesdell <wesdell17@gmail.com>
2024-04-19 15:33:06 -0500

    New model to work with data from Transactions by account id.

---
+++ budget_management_system/Models/GetTransactionByAccountModel.cs
@@ -0,0 +1,12 @@
+﻿using budget_management_system.Interfaces;
+
+namespace budget_management_system.Models
+{
+	public class GetTransactionsByAccountModel : ITransactionAccount
+	{
+		public int UserId { get; set; }
+		public int AccountId { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime FinishedAt { get; set; }
+	}
+}
