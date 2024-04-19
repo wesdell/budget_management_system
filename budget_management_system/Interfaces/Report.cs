1fb05e57ef1f3ef04ae40574e265a4d786853169
Wesdell <wesdell17@gmail.com>
2024-04-19 15:31:44 -0500

    New types for Report interface.

---
+++ budget_management_system/Interfaces/Report.cs
@@ -0,0 +1,23 @@
+﻿using budget_management_system.Models;
+
+namespace budget_management_system.Interfaces
+{
+	public interface IReportTransaction
+	{
+		DateTime CreatedAt { get; set; }
+		DateTime FinishedAt { get; set; }
+		decimal IncomeAmount { get; }
+		decimal ExpenseAmount { get; }
+		decimal Total { get; }
+		IEnumerable<TransactionsByDate> GroupedTransactions { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		class TransactionsByDate
+		{
+			DateTime TransactionDate { get; set; }
+			IEnumerable<TransactionModel> Transactions { get; set; }
+			decimal IncomeBalance;
+			decimal ExpenseBalance;
+		}
+	}
+}
+}
