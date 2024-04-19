a401ba6e02559414494ff11ccb636d1c588a99d5
Wesdell <wesdell17@gmail.com>
2024-04-19 15:34:44 -0500

    New endpoint to fire transaction report page.

--- budget_management_system/Controllers/AccountController.cs
+++ budget_management_system/Controllers/AccountController.cs
@@ -9,15 +9,17 @@ namespace budget_management_system.Controllers
 	public class AccountController : Controller
 	{
 		private readonly IAccountDBActions _accountService;
+		private readonly ITransactionDBActions _transactionService;
 		private readonly IMapper _mapper;
 		private readonly IAccountTypeDBActions _accountTypeService;
 		private readonly IUserDBActions _userService;
 
-		public AccountController(IAccountTypeDBActions accountType, IUserDBActions user, IAccountDBActions account, IMapper mapper)
+		public AccountController(IAccountTypeDBActions accountType, IUserDBActions user, IAccountDBActions account, ITransactionDBActions transaction, IMapper mapper)
 		{
 			this._accountTypeService = accountType;
 			this._userService = user;
 			this._accountService = account;
+			this._transactionService = transaction;
 			this._mapper = mapper;
 		}
 
@@ -26,6 +28,57 @@ namespace budget_management_system.Controllers
 		{
 			IEnumerable<AccountModel> accounts = await this._accountService.GetAccounts(this._userService.GetUserId());
 			List<AccountIndexViewModel> model = accounts.GroupBy(account => account.AccountType).Select(group => new AccountIndexViewModel { AccountType = group.Key, Accounts = group.AsEnumerable() }).ToList();
+			return View(model);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Report(int id, int month, int year)
+		{
+			AccountModel account = await this._accountService.GetAccountById(id, this._userService.GetUserId());
+			if (account is null)
+			{
+				return RedirectToAction("NotFound", "Home");
+			}
+
+			DateTime CreatedAt;
+			DateTime FinishedAt;
+
+			if (month <= 0 || month > 12 || year <= 1900)
+			{
+				CreatedAt = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+			}
+			else
+			{
+				CreatedAt = new DateTime(year, month, 1);
+			}
+
+			FinishedAt = CreatedAt.AddMonths(1).AddDays(-1);
+
+			GetTransactionsByAccountModel transactionsByAccount = new GetTransactionsByAccountModel
+			{
+				AccountId = id,
+				UserId = this._userService.GetUserId(),
+				CreatedAt = CreatedAt,
+				FinishedAt = FinishedAt
+			};
+
+			IEnumerable<TransactionModel> transactions = await this._transactionService.GetTransactionByAccount(transactionsByAccount);
+
+			ReportTransactionModel model = new ReportTransactionModel();
+			ViewBag.Account = account.Name;
+
+			IEnumerable<ReportTransactionModel.TransactionsByDate> transactionsByDate = transactions
+				.OrderByDescending(item => item.CreatedAt)
+				.GroupBy(item => item.CreatedAt)
+				.Select(transaction => new ReportTransactionModel.TransactionsByDate()
+				{
+					TransactionDate = transaction.Key,
+					Transactions = transaction.AsEnumerable()
+				});
+
+			model.GroupedTransactions = transactionsByDate;
+			model.CreatedAt = CreatedAt;
+			model.FinishedAt = FinishedAt;
 			return View(model);
 		}
 
