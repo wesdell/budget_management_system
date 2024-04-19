20c02ee404c62885dd16c25adf2667f7000425fb
Wesdell <wesdell17@gmail.com>
2024-04-19 15:31:20 -0500

    New types for Transaction and TransactionDBActions interfaces.

--- budget_management_system/Interfaces/Transaction.cs
+++ budget_management_system/Interfaces/Transaction.cs
@@ -5,12 +5,14 @@ namespace budget_management_system.Interfaces
 	public interface ITransaction
 	{
 		int Id { get; set; }
-		string Description { get; set; }
-		decimal Amount { get; set; }
-		DateTime CreatedAt { get; set; }
 		int UserId { get; set; }
 		int AccountId { get; set; }
 		int CategoryId { get; set; }
+		string Description { get; set; }
+		string Account { get; set; }
+		string Category { get; set; }
+		decimal Amount { get; set; }
+		DateTime CreatedAt { get; set; }
 		ETransactionType TransactionTypeId { get; set; }
 	}
 
@@ -18,7 +20,16 @@ namespace budget_management_system.Interfaces
 	{
 		Task CreateTransaction(TransactionModel transaction);
 		Task<TransactionModel> GetTransactionById(int id, int userId);
+		Task<IEnumerable<TransactionModel>> GetTransactionByAccount(GetTransactionsByAccountModel transactionByAccount);
 		Task UpdateTransaction(TransactionModel transaction, int previosAccountId, decimal previousAmount);
 		Task DeleteTransaction(int id);
+	}
+
+	public interface ITransactionAccount
+	{
+		int UserId { get; set; }
+		int AccountId { get; set; }
+		DateTime CreatedAt { get; set; }
+		DateTime FinishedAt { get; set; }
 	}
 }
